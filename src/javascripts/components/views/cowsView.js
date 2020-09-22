02ed926adae5b35057bc0f3f3e34a7a442c4939a
Liz <liz@Liz-s-Mac.local>
2020-09-21 21:20:44 -0500

    used read/get to create cow views

---
+++ src/javascripts/components/views/cowsView.js
@@ -0,0 +1,18 @@
+import cowData from '../../helpers/data/cowData';
+import card from '../cards/cowCard';
+
+const cowsView = () => {
+  cowData.getCows()
+    .then((response) => {
+      if (response.length) {
+        response.forEach((item) => {
+          $('#app').append(card.cowMaker(item));
+        });
+      } else {
+        $('#app').append('<h2>NO COWS</h2>');
+      }
+      // DO SOMETHING
+    });
+};
+
+export default { cowsView };
